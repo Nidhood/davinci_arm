a3b37c0d6f84e39a2a5b5e845331b231c00322b5
Nidhood <ivan.oroz34@gmail.com>
2026-03-30 11:44:15 -0500

    Refactor Davinci hardware interface and add JointsStates message support.

--- davinci_arm_gazebo_plugins/include/davinci_arm_gazebo_plugins/davinci_hardware_interface.hpp
+++ davinci_arm_gazebo_plugins/include/davinci_arm_gazebo_plugins/davinci_hardware_interface.hpp
@@ -10,44 +10,68 @@
 #include <gz/transport/Node.hh>
 
 #include "gz_ros2_control/gz_system_interface.hpp"
+#include "davinci_arm_msgs/msg/joints_states.hpp"
 
-class DavinciHardwareInterface : public gz_ros2_control::GazeboSimSystemInterface {
-private:
 
-    // Gazebo transport:
-    sim::EntityComponentManager *ecm_{nullptr};
-    std::map<std::string, sim::Entity> enable_joints_;
-    std::unique_ptr<gz::transport::Node> gz_node_;
-    gz::transport::Node::Publisher actuators_pub_;
+namespace davinci_arm_hardware_interface{
 
-    // ROS2 & Gazebo communication:
-    rclcpp::Node::SharedPtr nh_;
-    std::string actuators_topic_{"/davinci_arm/command/angles"};
-    std::string joint_name_{"arm_link_joint"};
+    class DavinciHardwareInterface : public gz_ros2_control::GazeboSimSystemInterface {
+    private:
 
-    // ROS2 communication:
+        // Gazebo transport:
+        sim::EntityComponentManager *ecm_{nullptr};
+        std::map<std::string, sim::Entity> enable_joints_;
+        std::unique_ptr<gz::transport::Node> gz_node_;
+        gz::transport::Node::Publisher actuators_pub_;
 
-    // Real motors characterization parameters loader:
-    void loadParameters(const hardware_interface::HardwareInfo &hardware_info);
-public:
-    DavinciHardwareInterface() = default;
-    ~DavinciHardwareInterface() = default;
+        // ROS2 & Gazebo communication:
+        rclcpp::Node::SharedPtr nh_;
+        std::string actuators_topic_{"/davinci_arm/command/angles"};
+        std::string joint_name_{"arm_link_joint"};
 
-    bool initSim(
-        rclcpp::Node::SharedPtr &model_nh,
-        std::map<std::string, sim::Entity> &joints,
-        const hardware_interface::HardwareInfo &hardware_info,
-        sim::EntityComponentManager &ecm,
-        unsigned int update_rate
-    ) override;
+        // ROS2 communication:
+        rclcpp::Publisher<davinci_arm_msgs::msg::JointsStates> joints_states_pub_;
 
-    hardware_interface::CallbackReturn on_init(
-        const hardware_interface::HardwareInfo &info) override;
-    hardware_interface::CallbackReturn on_configure(
-        const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_activate(
-        const rclcpp_lifecycle::State &previous_state) override;
-    hardware_interface::CallbackReturn on_deactivate(
-        const rclcpp_lifecycle::State &previous_state) override;
+        // Real motors characterization parameters loader:
+        void loadParameters(const hardware_interface::HardwareInfo &hardware_info);
 
-};
+    public:
+        RCLCPP_SHARED_PTR_DEFINITIONS(DavinciHardwareInterface)
+
+        bool initSim(
+            rclcpp::Node::SharedPtr &model_nh,
+            std::map<std::string, sim::Entity> &joints,
+            const hardware_interface::HardwareInfo &hardware_info,
+            sim::EntityComponentManager &ecm,
+            unsigned int update_rate
+        ) override;
+
+
+        // Hardware interface life cycle configurations:
+        hardware_interface::CallbackReturn on_init(
+            const hardware_interface::HardwareComponentInterfaceParams & params) override;
+
+        hardware_interface::CallbackReturn on_configure(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        hardware_interface::CallbackReturn on_activate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        hardware_interface::CallbackReturn on_deactivate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        hardware_interface::CallbackReturn on_cleanup(
+            const rclcpp_lifecycle::State & previous_state) override;
+
+        hardware_interface::CallbackReturn on_shutdown(
+            const rclcpp_lifecycle::State & previous_state) override;
+
+        // Hardware interface information exchange (Gazebo Harmonic <-> ROS2):
+        hardware_interface::return_type read(
+            const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        
+        hardware_interface::return_type write(
+            const rclcpp::Time & time, const rclcpp::Duration & period) override;
+    };
+
+} // namespace davinci_arm_hardware_interface
